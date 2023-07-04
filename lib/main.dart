72a34ed24b21ae8e325c86b53b931b36bfe25650
Pratik Lama <pratikpratiklama123@gmail.com>
2023-07-04 19:26:18 +0545

    add strech mode

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Silver App Bar Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -30,41 +31,70 @@ class MyApp extends StatelessWidget {
                   iconSize: 20,
                   color: Colors.black,
                 ),
-                actions: const [
-                  Icon(
-                    Icons.more_vert,
-                    size: 40,
-                    color: Colors.black,
-                  ),
-                ],
                 flexibleSpace: FlexibleSpaceBar(
-                  background: DecoratedBox(
-                    decoration: const BoxDecoration(
+                  stretchModes: const <StretchMode>[
+                    StretchMode.zoomBackground,
+                    StretchMode.fadeTitle,
+                  ],
+                  background: const DecoratedBox(
+                    decoration: BoxDecoration(
                       gradient: LinearGradient(
                         begin: Alignment.bottomCenter,
                         end: Alignment.topCenter,
                         colors: [
-                          Color.fromRGBO(
-                              10, 10, 10, 1), // Darker shade close to black
-                          Color.fromRGBO(
-                              20, 20, 20, 1), // Darker shade close to black
+                          Color.fromRGBO(10, 10, 10, 1),
+                          Color.fromRGBO(20, 20, 20, 1),
                           Color.fromRGBO(30, 30, 30, 1),
-                          Color.fromRGBO(
-                              31, 131, 68, 1), // Darker shade of green
+                          Color.fromRGBO(31, 131, 68, 1),
                           Color.fromRGBO(77, 175, 86, 1),
                           Color.fromRGBO(100, 201, 125, 1),
                         ],
                       ),
                     ),
-                    child: Center(
-                      child: SizedBox(
-                        height: (MediaQuery.of(context).size.height) * 0.2,
+                  ),
+                  centerTitle: true,
+                  title: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      // Row(
+                      //   mainAxisAlignment: MainAxisAlignment.center,
+                      //   children: [
+                      //     // Expanded(
+                      //     //   flex: 63,
+                      //     //   child: Container(
+                      //     //     // Add your search bar widget here
+
+                      //     //     color: Colors
+                      //     //         .red, // Replace with your search bar widget
+                      //     //   ),
+                      //     // ),
+                      //     // Container(
+                      //     //   color: Colors.black,
+                      //     //   height: 40,
+                      //     //   width: 200,
+                      //     // ),
+                      //     // Expanded(
+                      //     //   flex: 3,
+                      //     //   child: IconButton(
+                      //     //     onPressed: () {
+                      //     //       // Handle filter icon press
+                      //     //     },
+                      //     //     icon: const Icon(
+                      //     //       Icons.filter,
+                      //     //     ),
+                      //     //   ),
+                      //     // ),
+                      //   ],
+                      // ),
+                      SizedBox(
+                        height: 130,
+                        width: 120,
                         child: Image.network(
                           'https://images.pexels.com/photos/794494/pexels-photo-794494.jpeg?cs=srgb&dl=pexels-anthony-%F0%9F%93%B7%F0%9F%93%B9%F0%9F%99%82-794494.jpg&fm=jpg',
                           fit: BoxFit.cover,
                         ),
                       ),
-                    ),
+                    ],
                   ),
                 ),
               ),
