7ac3d64928d88938667d8c86d5adae8b4b8ea3d7
Pratik Lama <pratikpratiklama123@gmail.com>
2023-07-04 20:15:34 +0545

    make animation smooth

--- lib/main.dart
+++ lib/main.dart
@@ -21,19 +21,85 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.black,
           body: CustomScrollView(
             slivers: <Widget>[
-              SliverPersistentHeader(
+              SliverAppBar(
                 pinned: true,
-                delegate: _SliverAppBarDelegate(),
-              ),
-              SliverList(
-                delegate: SliverChildBuilderDelegate(
-                  (BuildContext context, int index) {
-                    return ListTile(
-                      textColor: Colors.white,
-                      title: Text('Item $index'),
+                expandedHeight: 300,
+                backgroundColor: const Color.fromRGBO(77, 175, 86, 1),
+                flexibleSpace: LayoutBuilder(
+                  builder: (BuildContext context, BoxConstraints constraints) {
+                    double percentage =
+                        (constraints.maxHeight - kToolbarHeight) / 200;
+                    percentage = percentage.clamp(0.0, 1.0);
+
+                    double imageHeight = 190 * (percentage);
+                    imageHeight = imageHeight.clamp(0.0, 190.0);
+
+                    return Stack(
+                      fit: StackFit.expand,
+                      children: [
+                        const DecoratedBox(
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              begin: Alignment.bottomCenter,
+                              end: Alignment.topCenter,
+                              colors: [
+                                Color.fromRGBO(0, 0, 0, 1),
+                                Color.fromRGBO(35, 55, 46, 1),
+                                Color.fromRGBO(31, 131, 68, 1),
+                                Color.fromRGBO(77, 175, 86, 1),
+                                Color.fromRGBO(100, 201, 125, 1),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            if (imageHeight > 0)
+                              SizedBox(
+                                height: imageHeight,
+                                width: 170 * percentage,
+                                child: Image.network(
+                                  'https://images.pexels.com/photos/794494/pexels-photo-794494.jpeg?cs=srgb&dl=pexels-anthony-%F0%9F%93%B7%F0%9F%93%B9%F0%9F%99%82-794494.jpg&fm=jpg',
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            if (imageHeight == 0)
+                              const Text(
+                                'Appbar',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                          ],
+                        ),
+                        Positioned(
+                          top: 8,
+                          left: 8,
+                          child: IconButton(
+                            icon: const Icon(Icons.arrow_back),
+                            color: Colors.black,
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                        ),
+                      ],
                     );
                   },
-                  childCount: 20,
+                ),
+              ),
+              SliverToBoxAdapter(
+                child: Column(
+                  children: List.generate(
+                    20,
+                    (index) => ListTile(
+                      textColor: Colors.white,
+                      title: Text('Item $index'),
+                    ),
+                  ),
                 ),
               ),
             ],
@@ -42,72 +108,4 @@ class MyApp extends StatelessWidget {
       ),
     );
   }
-}
-
-class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
-  @override
-  Widget build(
-      BuildContext context, double shrinkOffset, bool overlapsContent) {
-    const double maxExtent = 350.0;
-    const double minExtent = 0.0;
-    final double offset = shrinkOffset.clamp(minExtent, maxExtent);
-    final double percentage = 1.0 - (offset / maxExtent);
-
-    return SizedBox(
-      height: maxExtent - offset,
-      child: Stack(
-        fit: StackFit.expand,
-        children: [
-          const DecoratedBox(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.bottomCenter,
-                end: Alignment.topCenter,
-                colors: [
-                  Color.fromRGBO(0, 0, 0, 1),
-                  Color.fromRGBO(35, 55, 46, 1),
-                  Color.fromRGBO(31, 131, 68, 1),
-                  Color.fromRGBO(77, 175, 86, 1),
-                  Color.fromRGBO(100, 201, 125, 1),
-                ],
-              ),
-            ),
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              SizedBox(
-                height: 190 * percentage,
-                width: 170 * percentage,
-                child: Image.network(
-                  'https://images.pexels.com/photos/794494/pexels-photo-794494.jpeg?cs=srgb&dl=pexels-anthony-%F0%9F%93%B7%F0%9F%93%B9%F0%9F%99%82-794494.jpg&fm=jpg',
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ],
-          ),
-          Positioned(
-            top: 8,
-            left: 8,
-            child: IconButton(
-              icon: const Icon(Icons.arrow_back),
-              color: Colors.white,
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  @override
-  double get maxExtent => 350.0;
-
-  @override
-  double get minExtent => 0.0;
-
-  @override
-  bool shouldRebuild(_SliverAppBarDelegate oldDelegate) => true;
 }
